96a6b7354a2ae0ced7150e0ba350ea25d70768af
dizzyF2 <ifaris.ifaris172@gmail.com>
2026-03-15 21:42:44 +0200

    feat: replace date_of_birth with age and add validation

--- custom_addons/school_management_system/models/school_student.py
+++ custom_addons/school_management_system/models/school_student.py
@@ -1,13 +1,21 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 class SchoolStudent(models.Model):
     _name = 'school.student'
     _description = 'Student information'
 
     name = fields.Char(string='Student Name', required=True)
+    age = fields.Integer(string='Age')
     phone_number = fields.Integer(string='Phone Number', required=True)
-    date_of_birth = fields.Date(string='Birthday')
     address = fields.Text(string='Address')
     active = fields.Boolean(default=True)
 
     enrollment_ids = fields.One2many("school.enrollment", "student_id", string="Enrollment ID", required=True)
+    
+
+    @api.constrains("age")
+    def Check_age(self):
+        for rec in self:
+            if rec.age < 6 or rec.age > 12:
+                raise ValidationError("Age must be between 6 and 12")
