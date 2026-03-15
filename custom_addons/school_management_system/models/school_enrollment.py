74bb803d15425c43dfb2122429930da0ea1fdfec
dizzyF2 <ifaris.ifaris172@gmail.com>
2026-03-15 05:49:30 +0200

    ui: improve enrollment display name and field labels

--- custom_addons/school_management_system/models/school_enrollment.py
+++ custom_addons/school_management_system/models/school_enrollment.py
@@ -4,11 +4,12 @@ from odoo import fields, models
 
 class SchoolCourse(models.Model):
     _name = 'school.enrollment'
+    _rec_name = "student_id"
     _description = 'Enrollment Information'
 
     enrollment_date = fields.Date(string='enrollment Date', default=fields.Date.today())
     grade = fields.Float(string='Grade')
     active = fields.Boolean(default=True)
 
-    student_id = fields.Many2one("school.student", string="Student ID", required=True)
-    course_id = fields.Many2one("school.course", string="Course ID", required=True)
+    student_id = fields.Many2one("school.student", string="Student", required=True)
+    course_id = fields.Many2one("school.course", string="Course", required=True)
