1ba70c5345b023f268f7ca0e93453c0b6e7d3942
dizzyF2 <ifaris.ifaris172@gmail.com>
2026-03-15 23:33:23 +0200

    feat: understand & implement age validation using api onchange, api constrains, and sql constraints

--- custom_addons/school_management_system/models/school_student.py
+++ custom_addons/school_management_system/models/school_student.py
@@ -14,8 +14,25 @@ class SchoolStudent(models.Model):
     enrollment_ids = fields.One2many("school.enrollment", "student_id", string="Enrollment ID", required=True)
     
 
+    # this one will warn you because you enter invalid data and will not store it in the database
     @api.constrains("age")
     def Check_age(self):
         for rec in self:
             if rec.age < 6 or rec.age > 12:
                 raise ValidationError("Age must be between 6 and 12")
+
+    # this one will warn the user but will also store the data in the database
+    # @api.onchange("age")
+    # def _on_change_age(self):
+    #     if self.age < 6 or self.age > 12:
+    #         return {
+    #             'warning':{
+    #                 'title': 'Warning!',
+    #                 'message': 'Age must be between 6 and 12'
+    #             }
+    #         }
+
+    # _sql_constrain = [
+    #     # (choose name, operation sql, display message)
+    #     ('unique_name','unique(name)','Name is exists'),
+    # ]
